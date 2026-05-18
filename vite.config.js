eac63ac8afbbe806883e5448fb373263affc16a2
Ivan <ialiagaa@unc.edu.pe>
2026-05-17 23:29:34 -0500

    update general

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,116 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {
+  defineConfig
+} from "vite";
+
+import react
+  from "@vitejs/plugin-react";
+
+import {
+  VitePWA
+} from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
 
-  server: {
-    proxy: {
-      '/api': {
-        target:
-          'https://script.google.com',
+  plugins: [
 
-        changeOrigin: true,
+    react(),
 
-        secure: false,
+    VitePWA({
 
-        rewrite: (path) =>
-          path.replace(
-            /^\/api/,
-            ''
-          ),
-      },
+      registerType:
+        "autoUpdate",
+
+      includeAssets: [
+
+        "favicon.png",
+
+        "apple-touch-icon.png",
+      ],
+
+      manifest: {
+
+        name:
+          "Sistema Evidencias",
+
+        short_name:
+          "Evidencias",
+
+        description:
+          "Sistema de gestión de evidencias",
+
+        theme_color:
+          "#212529",
+
+        background_color:
+          "#212529",
+
+        display:
+          "standalone",
+
+        orientation:
+          "portrait",
+
+        start_url:
+          "/",
+
+        icons: [
+
+          {
+            src:
+              "/pwa-192.png",
+
+            sizes:
+              "192x192",
+
+            type:
+              "image/png",
+          },
+
+          {
+            src:
+              "/pwa-512.png",
+
+            sizes:
+              "512x512",
+
+            type:
+              "image/png",
+          },
+        ],
+         screenshots: [
+
+    {
+      src:
+        "/screenshot-desktop.png",
+
+      sizes:
+        "1280x720",
+
+      type:
+        "image/png",
+
+      form_factor:
+        "wide",
+
+      label:
+        "Sistema Evidencias Desktop"
     },
-  },
-})
+
+    {
+      src:
+        "/screenshot-mobile.png",
+
+      sizes:
+        "390x844",
+
+      type:
+        "image/png",
+
+      label:
+        "Sistema Evidencias Mobile"
+    }
+  ]
+      },
+    }),
+  ],
+});
